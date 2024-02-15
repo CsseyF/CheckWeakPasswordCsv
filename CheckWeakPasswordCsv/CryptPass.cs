814538d1bceee55c1e61cca968d974d7d882f34b
casey.olawuni <casey.olawuni@mottu.com.br>
2024-02-15 10:57:21 -0300

    Adicionar arquivos de projeto.

---
+++ CheckWeakPasswordCsv/CryptPass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckWeakPasswordCsv
+{
+    public class CryptPass
+    {
+        public static string CryptSalt(string senha, string salt)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(senha + salt);
+            SHA512 shaM = new SHA512Managed();
+            Byte[] hashedBytes = shaM.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+        public static void ValidarSenha(string senha, string salt, string senhaBanco)
+        {
+            var senhaSalt = CryptSalt(senha, salt);
+            Console.WriteLine(senhaSalt.Equals(senhaBanco));
+        }
+    }
+
+
+}
---
+++ CheckWeakPasswordCsv/Program.cs
@@ -0,0 +1,37 @@
+﻿using CheckWeakPasswordCsv;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+try
+{
+    List<string> weakPass = new List<string> { };
+
+    var usuarios = (from l in File.ReadAllLines("C:\\Users\\casey.olawuni\\usuario_202402141629.csv")
+                    let split = l.Split(",")
+                    where split.Length == 4
+                    select new
+                    {
+                        UsuarioId = split[0],
+                        Senha = split[1],
+                        Salt = split[2],
+                    }).ToList();
+
+    foreach (var user in usuarios)
+    {
+        var currentPassword = CryptPass.CryptSalt("123456", user.Salt.ToString());
+
+        if (currentPassword == user.Senha)
+        {
+            weakPass.Add(user.UsuarioId);
+        }
+    }
+    File.WriteAllLines("C:\\Users\\casey.olawuni\\listaSenhasFracas.txt", weakPass);
+    Console.WriteLine($"Processo concluido. {weakPass.Count()} ids de usuarios encontrados.");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+
